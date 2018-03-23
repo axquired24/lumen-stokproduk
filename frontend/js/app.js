3869ba3d06883379803a4fde08d232931a8d92aa
Albert Septiawan <albert.septiawan@ansvia.com>
2018-03-23 17:32:08 +0700

    add(stock management)

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -9,9 +9,84 @@ var app = new Vue({
     el: '#app',
     data: {
         pageState: HOME_STR,
-        message: 'Hello Vue!'
+        productCat: {
+            data: []
+        },
+        productList: {
+            data: []
+        },
+        currentPage: {
+            stock: 1
+        },
+        currentSearch: {
+            stock: ''
+        },
+        isLoading: {
+            stock: false
+        }
     },
     methods: {
+        loadProductCat: function () {
+            axios.post(config.api + '/productcat')
+            .then(function(response) {
+                app.productCat = response.data;
+            })
+            .catch(function(error) {
+                console.log('ERROR: ' + config.api + '/productcat')
+                console.log(error);
+            })
+        },
+        loadProduct: function (page=1, q='') {
+            this.isLoading.stock = true;
+            let hitUrl = config.api + '/product';
+            if(q != '') {
+                if (q.length < 3) {
+                    alert('Keyword pencarian harus lebih dari 3 karakter')
+                    this.isLoading.stock = false;
+                    return
+                }
+                hitUrl = config.api + '/product/search';
+            }
+            axios.post(hitUrl, {
+                page: page,
+                q: q
+            })
+                .then(function (response) {
+                    app.productList = response.data.data;
+                    app.currentPage.stock = page;
+                    app.isLoading.stock = false;                    
+                })
+                .catch(function (error) {
+                    console.log('ERROR: ' + hitUrl)
+                    console.log(error);
+                    app.isLoading.stock = false;                    
+                })
+        },
+        updateStok: function(product) {
+            const productDetail = product.name + "(tipe "+ product.product_cat.name +")";
+            const updateProductUrl = config.api + "/product/edit";
+            let newStock = prompt("Update stok: " + productDetail, product.stock);
+            newStock = parseInt(newStock);
+            if (! newStock) {
+                // false if nothing input
+                return
+            }
+
+            this.isLoading.stock = true;            
+            axios.post(updateProductUrl, {
+                id: product.id,
+                stock: newStock
+            })
+                .then(function (response) {
+                    app.loadProduct()
+                })
+                .catch(function (error) {
+                    console.log('ERROR: ' + updateProductUrl)
+                    console.log(error);
+                    app.isLoading.stock = false;
+                })
+            
+        },
         catShow: function () {
             this.pageState = CATEGORY_STR;
         },
@@ -47,6 +122,24 @@ var app = new Vue({
         },
         homeView: function () {
             return this.pageState == HOME_STR;
+        },
+        searchStock: function() {
+            this.isLoading.stock = true;            
+            delay(function () {
+                app.loadProduct(1, app.currentSearch.stock);
+            }, 1000);
         }
+    },
+    mounted: function() {
+        this.loadProductCat();
+        this.loadProduct();
     }
 })
+
+var delay = (function () {
+    var timer = 0;
+    return function (callback, ms) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
